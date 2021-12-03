e49f70bb4d9d276cf041f53cdad57c8ab0627ad9
Michael Ilyin <milyin@gmail.com>
2021-12-03 23:15:12 +0300

    pub parsed

--- async_object_derive/src/lib.rs
+++ async_object_derive/src/lib.rs
@@ -3,12 +3,7 @@ extern crate proc_macro;
 use core::iter::Extend;
 use quote::quote;
 use std::iter::once;
-use syn::{self, parse_macro_input};
-
-enum ObjectMacroTitle {
-    AsyncObject(String),
-    AsyncObjectEvents(String),
-}
+use syn::{self, parse::Parse, parse_macro_input, Ident, Token, Visibility};
 
 fn append(
     item: proc_macro::TokenStream,
@@ -19,16 +14,34 @@ fn append(
     result.into()
 }
 
+struct AsyncObjectDecl {
+    visibility: syn::Visibility,
+    ident: syn::Ident,
+}
+
+impl Parse for AsyncObjectDecl {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let look = input.lookahead1();
+        let visibility = if look.peek(Token![pub]) {
+            input.parse()?
+        } else {
+            syn::Visibility::Inherited
+        };
+        let ident = input.parse()?;
+        Ok(AsyncObjectDecl { visibility, ident })
+    }
+}
+
 #[proc_macro_attribute]
 pub fn async_object(
     attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    let ident: syn::Ident = parse_macro_input!(attr);
-    append(
-        item,
-        quote! {
-            struct #ident;
-        },
-    )
+    let decl: AsyncObjectDecl = parse_macro_input!(attr);
+    let visibility = decl.visibility;
+    let ident = decl.ident;
+    let quote = quote! {
+        #visibility struct #ident;
+    };
+    append(item, quote)
 }
--- async_object_derive/tests/derive.rs
+++ async_object_derive/tests/derive.rs
@@ -4,9 +4,15 @@ use async_object_derive::async_object;
 struct TestImpl {
     foo: usize,
 }
+#[async_object(pub Test2)]
+struct Test2Impl {
+    foo: usize,
+}
 
 #[test]
 fn derive_test() {
     let _ = TestImpl { foo: 0 };
     let _ = Test;
+    let _ = Test2Impl { foo: 0 };
+    let _ = Test2;
 }
