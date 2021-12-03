a2e71f9600604b6cd932e231207bf3f2ef4ff8a6
Michael Ilyin <milyin@gmail.com>
2021-12-03 23:40:25 +0300

    CArc added

--- async_object_derive/src/lib.rs
+++ async_object_derive/src/lib.rs
@@ -3,7 +3,7 @@ extern crate proc_macro;
 use core::iter::Extend;
 use quote::quote;
 use std::iter::once;
-use syn::{self, parse::Parse, parse_macro_input, Ident, Token, Visibility};
+use syn::{self, parse::Parse, parse_macro_input, DeriveInput, Ident, Token, Visibility};
 
 fn append(
     item: proc_macro::TokenStream,
@@ -38,10 +38,23 @@ pub fn async_object(
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
     let decl: AsyncObjectDecl = parse_macro_input!(attr);
-    let visibility = decl.visibility;
-    let ident = decl.ident;
+    let item1 = item.clone();
+    let derive_input: DeriveInput = parse_macro_input!(item1);
+    let async_object_visibility = decl.visibility;
+    let async_object_ident = decl.ident;
+    let wrapped_object_ident = derive_input.ident;
+    let wrapped_object_visibility = derive_input.vis;
     let quote = quote! {
-        #visibility struct #ident;
+        #async_object_visibility struct #async_object_ident {
+            carc: async_object::CArc<#wrapped_object_ident>
+        }
+        impl #async_object_ident {
+            #wrapped_object_visibility fn create(object: #wrapped_object_ident) -> Self {
+                Self {
+                    carc: async_object::CArc::new(object)
+                }
+            }
+        }
     };
     append(item, quote)
 }
--- async_object_derive/tests/derive.rs
+++ async_object_derive/tests/derive.rs
@@ -1,18 +1,14 @@
 use async_object_derive::async_object;
 
 #[async_object(Test)]
-struct TestImpl {
-    foo: usize,
-}
+struct TestImpl {}
 #[async_object(pub Test2)]
-struct Test2Impl {
-    foo: usize,
-}
+struct Test2Impl {}
 
 #[test]
 fn derive_test() {
-    let _ = TestImpl { foo: 0 };
-    let _ = Test;
-    let _ = Test2Impl { foo: 0 };
-    let _ = Test2;
+    let test = TestImpl {};
+    let _ = Test::create(test);
+    let test2 = Test2Impl {};
+    let _ = Test2::create(test2);
 }
