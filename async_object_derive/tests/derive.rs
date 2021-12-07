81cabb1dd86058df0c7ac926cc1a82be45801cee
Michael Ilyin <milyin@gmail.com>
2021-12-07 21:09:29 +0300

    tests added, clone impl added

--- async_object_derive/tests/derive.rs
+++ async_object_derive/tests/derive.rs
@@ -1,28 +1,82 @@
 use async_object_derive::{async_object_decl, async_object_impl, async_object_with_events_decl};
-use futures::executor::ThreadPool;
+use futures::StreamExt;
+use futures::{executor::LocalPool, task::SpawnExt};
 
 #[async_object_decl(Test, WTest)]
-struct TestImpl {}
-#[async_object_with_events_decl(pub Test2, pub WTest2)]
-struct Test2Impl {}
+struct TestImpl {
+    foo: usize,
+}
+
+impl TestImpl {
+    pub fn new() -> Self {
+        Self { foo: 0 }
+    }
+}
 
 #[async_object_impl(Test, WTest)]
 impl TestImpl {
-    fn test(&self, foo: usize) -> bool {
-        true
+    fn test(&self) -> usize {
+        self.foo
     }
-    fn test_mut(&mut self, foo: usize) -> bool {
-        true
+    fn test_mut(&mut self, foo: usize) {
+        self.foo = foo;
     }
 }
 
 #[test]
-fn derive_test() {
-    let pool = ThreadPool::new().unwrap();
-    let test = TestImpl {};
-    let mut test = Test::create(test);
-    let test2 = Test2Impl {};
-    let test2 = Test2::create(test2, pool);
-    test.test(42);
+fn async_object_decl_test() {
+    let mut test = Test::create(TestImpl::new());
+    let mut wtest = test.downgrade();
     test.test_mut(42);
+    assert!(test.test() == 42);
+    assert!(wtest.upgrade().is_some());
+    assert!(wtest.test_mut(43) == Some(()));
+    assert!(wtest.test().unwrap() == 43);
+}
+
+#[async_object_with_events_decl(pub Test2, pub WTest2)]
+struct Test2Impl {
+    foo: usize,
+}
+
+impl Test2Impl {
+    pub fn new() -> Self {
+        Self { foo: 0 }
+    }
+}
+
+#[async_object_impl(Test2, WTest2)]
+impl Test2Impl {
+    fn test(&self) -> usize {
+        self.foo
+    }
+    fn test_mut(&mut self, foo: usize) {
+        self.foo = foo;
+    }
+}
+
+#[test]
+fn async_object_with_events_decl_test() {
+    let mut pool = LocalPool::new();
+    let mut test = Test2::create(Test2Impl::new(), pool.spawner()).unwrap();
+    let mut wtest = test.downgrade();
+    test.test_mut(42);
+    assert!(test.test() == 42);
+    assert!(wtest.upgrade().is_some());
+    assert!(wtest.test_mut(43) == Some(()));
+    assert!(wtest.test().unwrap() == 43);
+    pool.spawner()
+        .spawn({
+            let mut test = test.clone();
+            let mut stream = test.create_event_stream::<usize>();
+            async move {
+                let v = stream.next().await;
+                assert!(*v.unwrap().as_ref() == 44);
+                test.test_mut(45);
+            }
+        })
+        .unwrap();
+    test.send_event(44usize);
+    pool.run_until_stalled();
+    assert!(test.test() == 45);
 }
